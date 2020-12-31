c9e03e5edd78c85014dc00cdff0f3beb78834b52
Michael Nti <michaelnti@Michaels-MBP-2.home>
2020-12-31 13:28:12 -0500

    Worked with hash function MD5 && replaced it with bcrypt

--- app.js
+++ app.js
@@ -5,8 +5,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+// const encrypt = require('mongoose-encryption');
+// const md5 = require('md5');
+const bcrypt = require('bcrypt');
 const app = express();
+
+
+//Bycrypt:similar to MD5: more secure way to hash passwords. through hash function
+const saltRounds = 10;
 
 //render public folder
 app.use(express.static('public'));
@@ -24,7 +30,7 @@ const userSchema = new mongoose.Schema({
 });
 
 //This will encryp entire DB. However, we only want to encrypt password. Specify that by using encruyptedField
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']})
+//userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']})
 
 //Creating a model
 const User = mongoose.model('User',userSchema);
@@ -41,20 +47,24 @@ app.get('/register',(req,res)=>{
     res.render('register');
 })
 
-app.post('/register', (req,res)=>{
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
+app.post('/register', function(req,res){
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        // Store hash in your password DB.
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        //saving new user in db
+        newUser.save((err)=>{
+            if(!err){
+                //if there is no error after logging in render secret page
+                res.render('secrets');
+            }else{
+                console.log(err);
+            }
+        });
     });
-
-    newUser.save((err)=>{
-        if(!err){
-            //if there is no error after logging in render secret page
-            res.render('secrets');
-        }else{
-            console.log(err);
-        }
-    });
+    
     
 });
 
@@ -71,11 +81,17 @@ app.post('/login', (req,res)=>{
         }else{
             //if there is a object of foundUser which is not null
             if(foundUser){
-                if(foundUser.password === password){
-                    console.log(password)
-                    //if user's password matches the password in database then render secrets page
-                    res.render('secrets');
-                }
+                // if(foundUser.password === password){
+                //     console.log(password)
+                //     //if user's password matches the password in database then render secrets page
+                //     res.render('secrets');
+                // }
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    // result == true
+                    if(result === true){
+                        res.render('secrets');
+                    }
+                });
             }
         }
     })
